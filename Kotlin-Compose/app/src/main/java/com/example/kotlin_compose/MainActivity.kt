275e682cf9382f90b6e2a31d1fdbed1042a96202
Emil Djurson <emidj236@student.liu.se>
2025-11-19 13:34:14 +0100

    Update MainActivity.kt

--- Kotlin-Compose/app/src/main/java/com/example/kotlin_compose/MainActivity.kt
+++ Kotlin-Compose/app/src/main/java/com/example/kotlin_compose/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.example.kotlin_compose
 
-import android.R
+import android.app.Activity
 import android.os.Bundle
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
-import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,44 +33,83 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.kotlin_compose.ui.theme.KotlinComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            KotlinComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(innerPadding),
-                        horizontalArrangement = Arrangement.Center
+            SetStatusBarIconsWhite()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .windowInsetsTopHeight(WindowInsets.statusBars)
+                    .background(Color(0xFF153d37)))
+                Header()
+                Spacer(modifier = Modifier.height(24.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            DiagramImage()
-                            ButtonGroup()
-                            Spacer(modifier = Modifier.height(24.dp))
-                            EmailInputLinearGroup()
-                        }
+                        DiagramImage()
+                        ButtonGroup()
+                        Spacer(modifier = Modifier.height(24.dp))
+                        EmailInputLinearGroup()
                     }
                 }
             }
         }
+
+    }
+}
+
+@Composable
+fun SetStatusBarIconsWhite() {
+    val view = LocalView.current
+    SideEffect {
+        val window = (view.context as Activity).window
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false
+    }
+}
+
+
+@Composable
+fun Header(){
+    Box(
+        modifier = Modifier.fillMaxWidth()
+            .background(Color(0xFF2A7E72))
+            .height(56.dp),
+        contentAlignment = Alignment.Center
+    ) {
+       Text(
+           text = "Kotlin and Compose",
+           fontSize = 20.sp,
+           color = Color.White
+       )
     }
 }
 
@@ -87,12 +133,13 @@ fun AppCompatStyledButton(buttonText: String) {
 @Composable
 fun DiagramImage() {
     Image(
-        painter = painterResource(id = R.drawable),
+        painter = painterResource(R.drawable.circleimage),
         contentDescription = "diagram",
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
             .padding(bottom = 48.dp)
+            .size(200.dp)
     )
 }
 
